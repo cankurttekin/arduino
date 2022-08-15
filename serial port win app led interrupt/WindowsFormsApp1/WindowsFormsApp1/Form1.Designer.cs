a850d51b005dbc36a0136e199651ebbabb143d70
can kurttekin <cankurttekin@gmail.com>
2022-08-15 13:23:28 +0300

    Add files via upload

--- serial port win app led interrupt/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ serial port win app led interrupt/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -37,12 +37,13 @@
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 26);
+            this.label1.Location = new System.Drawing.Point(12, 19);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(37, 13);
             this.label1.TabIndex = 6;
@@ -51,7 +52,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 55);
+            this.label2.Location = new System.Drawing.Point(12, 51);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(37, 13);
             this.label2.TabIndex = 7;
@@ -59,78 +60,92 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(50, 21);
+            this.button1.Location = new System.Drawing.Point(55, 14);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 8;
-            this.button1.Text = "aç";
+            this.button1.Text = "AÇ";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(138, 21);
+            this.button2.Location = new System.Drawing.Point(143, 14);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 9;
-            this.button2.Text = "kapa";
+            this.button2.Text = "KAPAT";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(50, 50);
+            this.button3.Location = new System.Drawing.Point(55, 46);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 10;
-            this.button3.Text = "aç";
+            this.button3.Text = "AÇ";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(138, 50);
+            this.button4.Location = new System.Drawing.Point(143, 46);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 11;
-            this.button4.Text = "kapa";
+            this.button4.Text = "KAPAT";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(138, 84);
+            this.button5.Location = new System.Drawing.Point(143, 78);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 14;
-            this.button5.Text = "kapa";
+            this.button5.Text = "KAPAT";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(50, 84);
+            this.button6.Location = new System.Drawing.Point(55, 78);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(75, 23);
             this.button6.TabIndex = 13;
-            this.button6.Text = "aç";
+            this.button6.Text = "AÇ";
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(7, 89);
+            this.label3.Location = new System.Drawing.Point(12, 83);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(37, 13);
             this.label3.TabIndex = 12;
             this.label3.Text = "AUTO";
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 148);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(232, 191);
+            this.richTextBox1.TabIndex = 16;
+            this.richTextBox1.Text = "";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(230, 233);
+            this.ClientSize = new System.Drawing.Size(232, 339);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.label3);
@@ -140,7 +155,9 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -158,6 +175,7 @@
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- serial port win app led interrupt/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ serial port win app led interrupt/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -1,22 +1,15 @@
 ﻿using System;
-
 using System.IO.Ports;
-
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
         SerialPort _serialPort;
-        
+        private string ReceivedData;
+
         public Form1()
         {
             InitializeComponent();
@@ -26,39 +19,63 @@ namespace WindowsFormsApp1
                 _serialPort = new SerialPort("COM4", 9600);
                 _serialPort.Open();
             }
+
+            _serialPort.DataReceived += new SerialDataReceivedEventHandler(_serialPort_DataReceived);
+            
         }
 
-        // LED 1 ON
+
+        private void _serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e) {
+            //Thread.Sleep(200);
+            
+            ReceivedData = _serialPort.ReadLine();  // bufferdan veri okuma
+            if (richTextBox1.InvokeRequired)
+            {
+                richTextBox1.Invoke(new MethodInvoker(delegate {
+
+                    richTextBox1.Text += ReceivedData.ToString() + "\n";
+                    
+                    
+                    
+                }));
+                
+                
+                
+            }            
+        }
+
+
+        // led 1 on
         private void button1_Click(object sender, EventArgs e)
         {
             SerialWrite("1");
         }
 
-        // LED 1 OFF
+        // led 1 off
         private void button2_Click(object sender, EventArgs e)
         {
             SerialWrite("2");
         }
 
-        // LED 2 ON
+        // led 2 on
         private void button3_Click(object sender, EventArgs e)
         {
             SerialWrite("3");
         }
 
-        // LED 2 OFF
+        // led 2 off
         private void button4_Click(object sender, EventArgs e)
         {
             SerialWrite("4");
         }
 
-        // Blink 2 leds ON
+        // blink leds on
         private void button6_Click(object sender, EventArgs e)
         {
             SerialWrite("5");
         }
 
-        // Blink 2 leds OFF
+        // blink leds off
         private void button5_Click(object sender, EventArgs e)
         {
             SerialWrite("6");
@@ -69,16 +86,16 @@ namespace WindowsFormsApp1
         {
             if (_serialPort != null && _serialPort.IsOpen)
             {
-                _serialPort.Write(value);
+                _serialPort.WriteLine(value);
             }
+            
         }
+
         
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-
-        
     }
 }
